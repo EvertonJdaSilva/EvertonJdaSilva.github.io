21bc947ded46a810d0bc9df03d0ea51de240c03f
Everton Silva <91961660+EvertonJdaSilva@users.noreply.github.com>
2025-07-15 17:51:53 +0100

    update talk map

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,82 +1,82 @@
 var addressPoints = [
   [
     "EUROPT 2025 – 22nd Conference on Advances in Continuous Optimization<br />University of Southampton; Southampton, UK",
-     50.93535,
-     -1.39404
+    50.93535,
+    -1.39404
   ],
   [
     "2nd Derivative-free Optimization Symposium (DFOS 2024) <br /> University of Padova; Padova, IT",
-     45.4067383,
-     11.877394
+    45.4067383,
+    11.877394
   ],
   [
     "25th International Symposium on Mathematical Programming (ISMP 2024) <br /> Palais des congrès de Montréal; Montreal, CA",
-     45.505745,
-     -73.559503
+    45.505745,
+    -73.559503
   ],
   [
     "EUROPT 2023 – 20th Workshop on Advances in Continuous Optimization <br /> Corvinus University of Budapest; Budapest, HU",
-     47.4862802,
-     19.0581091
+    47.4862802,
+    19.0581091
   ],
   [
     "Optimization 2023 <br /> University of Aveiro; Aveiro, PT",
-     40.6311809,
-     -8.6574101
+    40.6311809,
+    -8.6574101
   ],
   [
     "Derivative-Free Optimization Symposium (DFOS 2022) <br /> University of British Columbia; Kelowna, CA", 
-     49.9421752,
-     -119.3990282
+    49.9421752,
+    -119.3990282
   ],
   [
     "EUROPT 2022 – 19th Workshop on Advances in Continuous Optimization <br /> NOVA School for Sciences and Technology; Caparica, PT",
-     38.6602924,
-     -9.205052
+    38.6602924,
+    -9.205052
   ],
   [
     "French-German-Portuguese Conference on Optimization 2022 <br /> University of Porto; Porto, PT",
-     41.1754856,
-     -8.5984686
+    41.1754856,
+    -8.5984686
   ],
   [
     "Lisbon Young Mathematicians Conference 2022 <br /> University of Lisbon; Lisbon, PT",
-     38.7519172,
-     -9.1586458
+    38.7519172,
+    -9.1586458
   ],
   [
     "J3M - Jornada de Matemática, Matemática Aplicada e Educação Matemática 2017 <br /> Federal University of Paraná; Curitiba, BR",
-     -25.451044,
-     -49.233127
+    -25.451044,
+    -49.233127
   ],
   [
     "J3M - Jornada da Matemática, Matemática Aplicada e Educação Matemática 2016  <br /> Federal University of Paraná; Curitiba, BR",
-     -25.451044,
-     -49.233127
+    -25.451044,
+    -49.233127
   ],
   [
     "LICENCIAR: 20 Anos Trocando Experiências 2015 <br /> Federal University of Paraná; Curitiba, BR",
-     -25.4129671,
-     -49.2487869
+    -25.4129671,
+    -49.2487869
   ],
   [
     "SIEPE - Semana Integrada de Ensino, Pesquisa e Extensão 2015 <br /> Federal University of Paraná; Curitiba, BR",
-     -25.451044,
-     -49.233127
+    -25.451044,
+    -49.233127
   ],
   [
     "Seminar at the Institute of Mathematics, Statistics and Scientific Computing 2025 <br /> University of Campinas; Campinas, BR", 
-     -22.8174449,
-     -47.0712132
+    -22.8174449,
+    -47.0712132
   ],
   [
     "Seminar at the Department of Computer, Control and Management Engineering 2023 <br /> Sapienza University of Rome; Rome, IT", 
-     42.2075781,
-     12.4811751
+    42.2075781,
+    12.4811751
   ],
   [
     "Seminar at the Postgraduate Program in Mathematics (PPGM-UFPR 2022) <br /> Federal University of Paraná; Curitiba, BR", 
-     -25.4129671,
-     -49.2487869
-  ],
+    -25.4129671,
+    -49.2487869
+  ]
 ];
